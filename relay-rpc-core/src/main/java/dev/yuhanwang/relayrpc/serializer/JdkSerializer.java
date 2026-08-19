b0bb042372b4c6e03a8669772a4f16d959f511c8
J-YuhanWang <wangyuhan6459@gmail.com>
2026-08-19 17:41:08 +0100

    refactor(serializer): optimize JdkSerializer with try-with-resources and explicit error handling

--- relay-rpc-core/src/main/java/dev/yuhanwang/relayrpc/serializer/JdkSerializer.java
+++ relay-rpc-core/src/main/java/dev/yuhanwang/relayrpc/serializer/JdkSerializer.java
@@ -6,23 +6,23 @@ public class JdkSerializer implements Serializer{
     @Override
     public <T> byte[] serialize(T object) throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-        objectOutputStream.writeObject(object);
-        objectOutputStream.close();
+        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)){
+            objectOutputStream.writeObject(object);
+            objectOutputStream.flush();
 
-        return outputStream.toByteArray();
+            return outputStream.toByteArray();
+
+        }
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <T> T deserialize(byte[] bytes, Class<T> type) throws IOException {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-        try{
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);){
             return (T)objectInputStream.readObject();
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }finally {
-            objectInputStream.close();
+            throw new RuntimeException("Class not found during deserialization", e);
         }
     }
 }
